7f0e784dbf380d47e1549c80833e2dc82fd4a267
Leonneke <lmadaame@avans.nl>
2015-03-31 14:25:38 +0200

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,38 +1,56 @@
 <?php
 
-Route::get('/', 'MainController@index');
+//Route::get('/', 'MainController@index');
+Route::get('/', array(
+    'as' => 'home',
+    'uses' => 'HomeController@home'
+));
 
-Route::get('home', 'MainController@index');
+Route::get('/about', array(
+   'as' => 'about',
+   'uses' => 'MenuController@about'
+));
 
-Route::get('login', 'MainController@login');
+Route::get('/services', array(
+   'as' => 'services',
+   'uses' => 'MenuController@services'
+));
 
+Route::get('/contact', array(
+   'as' => 'contact',
+   'uses' => 'MenuController@contact'
+));
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+    'auth' =>  'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
 
 /*
-| Nog niet ingelogde groep
- */
+# Nog niet ingelogde groep
+ 
 Route::group(array('before' => 'guest'), function (){
-    /*
-    | csrf protection??
-    */
+    
+    # csrf protection??
+    
     Route::group(array('before' => 'csrf'), function (){
 
     });
 
-    /*
-    | account aanmaken post
-    */
+    
+    # account aanmaken post    
     Route::post('/account/create', array(
         'as' => 'account-create-post',
         'uses' => 'AccountController@postCreate'
     ));
 
-
-    /*
-    | account aanmaken get
-    */
+    
+    # account aanmaken get    
     Route::get('/account/create', array(
         'as' => 'account-create',
         'uses' => 'AccountController@getCreate'
     ));
 });
-
+*/
--- app/views/navigation.blade.php
+++ app/views/navigation.blade.php
@@ -1,40 +1,45 @@
+<!-- (OUD kaspar)
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="home">Start Bootstrap</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                     <li>
-                        <a href="login" class="button" id="toggle-login">Login</a>
-                     </li>
-                     @if(Auth::check())
+    <div class="container"> 
+-->
 
-                     @else
-                          <li>
-                                <a href="{{ URL::route('account-create') }}" class="button" id="toggle-login">Register</a>
-                          </li>
-                     @endif
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+<!--
+<nav class="navbar navbar-default">-->
+<nav class="navbar navbar-inverse" role="navigation">
+    <div class="container-fluid">       
+        
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle Navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ url('/') }}">Home</a>
+	</div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+		<!--<li><a href="{{ url('/') }}">Home</a></li>-->
+                <li><a href="{{ url('/about') }}">About</a></li>
+                <li><a href="{{ url('/services') }}">Services</a></li>
+                <li><a href="{{ url('/contact') }}">Contact</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+            @if (Auth::guest())
+		<li><a href="{{ url('/auth/login') }}">Login</a></li>
+                <li><a href="{{ url('/auth/register') }}">Register</a></li>
+            @else
+		<li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                      	<ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+			</ul>
+                </li>
+            @endif
+            </ul>
         </div>
-        <!-- /.container -->
-    </nav>
+    </div>
+</nav>
+
