91ced2facc062e22c62b2f57886f241f378999cb
Leonneke <lmadaame@avans.nl>
2015-04-18 14:51:40 +0200

    framework (eerste opzet; nog met errors)

---
+++ config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Config file
+ * 
+ * @author Leon Daamen
+ */
+
+$config['title'] = '[subtitle | Webshop';
+
+$config["db_name"] = "lmadaame_db2";
+$config["db_user"] = "lmadaame";
+$config["db_pass"] = "Ab12345";
+$config["db_host"] = "databases.aii.avans.nl";
+$config["db_port"] = "3306";
+
+$config["path"] = "/WS2_Eindopdracht";
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+
+# Project
+	$settings["title"] = "[subtitle] | Webshop";
+
+	# Database
+	$settings["db_name"] = "lmadaame_db2";
+	$settings["db_user"] = "lmadaame";
+	$settings["db_pass"] = "Ab12345";
+	$settings["db_host"] = "databases.aii.avans.nl";
+	$settings["db_port"] = "3306";
+
+	# Path
+	$settings["path"] = "/WS2_Eindopdracht";
+
+//	# Errors
+//	error_reporting(E_ALL);
+//	ini_set('display_errors', '1');
+	
+?>
